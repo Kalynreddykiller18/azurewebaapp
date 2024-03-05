9a540a9431b052b16e3dc03da28cd4da5b89ad11
Kalyanreddykiller18 <kalyanbejjanki9010@gmail.com>
2024-03-05 20:36:28 +0530

    ..

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
+// vite.config.js
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
     esbuild: {
         target: "es2015",
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            format: "esm",
+        },
     },
 });
